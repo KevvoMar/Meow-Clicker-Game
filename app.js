e88f9e9e5f57e79ed937896e620987318330762b
KevvoMar <marrerokev25@GMAIL.COM>
2026-03-23 15:27:26 -0400

    meow game v1

---
+++ app.js
@@ -0,0 +1,211 @@
+let meows = 0;
+let meowsPerClick = 1;
+let meowsPerSecond = 0;
+let upgradeOneCost = 20;
+let upgradeTwoCost = 50;
+let upgradeThreeCost = 500;
+let upgradeFourCost = 2500;
+let upgradeFiveCost = 4000;
+let upgradeSixCost = 10000;
+
+const currency  = document.querySelector(".currency");
+const currencyPerSecond = document.querySelector(".currency__second")
+const catImg = document.querySelector(".clicker__img")
+
+//update shop and currency values
+
+function updateShopUI() {
+    document.querySelector(".upgrade__one--cost").textContent = upgradeOneCost;
+    document.querySelector(".upgrade__two--cost").textContent = upgradeTwoCost;
+    document.querySelector(".upgrade__three--cost").textContent = upgradeThreeCost;
+    document.querySelector(".upgrade__four--cost").textContent = upgradeFourCost;
+    document.querySelector(".upgrade__five--cost").textContent = upgradeFiveCost;
+    document.querySelector(".upgrade__six--cost").textContent = upgradeSixCost;
+
+}
+
+function updateCurrency() {
+    currency.textContent = "Meows: " + meows;
+    currencyPerSecond.textContent = "Meows Per Second: " + meowsPerSecond;
+    updateShopUI();
+}
+
+//upgrades
+
+document.querySelector(".upgrade__one").addEventListener("click", () => {
+    if (meows >= upgradeOneCost) {
+        meows -= upgradeOneCost;
+        meowsPerClick++;
+        upgradeOneCost = Math.floor(upgradeOneCost * 1.2);
+        updateCurrency();
+    }
+    else {
+        const warning = document.createElement("p");
+        warning.textContent = "Not Enough Meows!";
+        warning.classList.add("warning");
+        document.body.appendChild(warning);
+
+        setTimeout(() => warning.remove(), 1000);
+
+    }
+});
+
+document.querySelector(".upgrade__two").addEventListener("click", () => {
+    if (meows >= upgradeTwoCost) {
+        meows -= upgradeTwoCost;
+        meowsPerSecond++;
+        upgradeTwoCost = Math.floor(upgradeTwoCost * 1.4);
+        updateCurrency();
+    }
+    else {
+        const warning = document.createElement("p");
+        warning.textContent = "Not Enough Meows!";
+        warning.classList.add("warning");
+        document.body.appendChild(warning);
+
+        setTimeout(() => warning.remove(), 1000);
+
+    }
+});
+
+document.querySelector(".upgrade__three").addEventListener("click", () => {
+    if (meows >= upgradeThreeCost) {
+        meows -= upgradeThreeCost;
+        meowsPerClick += 2;
+        upgradeThreeCost = Math.floor(upgradeThreeCost * 1.5);
+        updateCurrency();
+    }
+    else {
+        const warning = document.createElement("p");
+        warning.textContent = "Not Enough Meows!";
+        warning.classList.add("warning");
+        document.body.appendChild(warning);
+
+        setTimeout(() => warning.remove(), 1000);
+
+    }
+});
+
+document.querySelector(".upgrade__four").addEventListener("click", () => {
+    if (meows >= upgradeFourCost) {
+        meows -= upgradeFourCost;
+        meowsPerSecond += 2;
+        upgradeFourCost = Math.floor(upgradeFourCost * 1.5);
+        updateCurrency();
+    }
+    else {
+        const warning = document.createElement("p");
+        warning.textContent = "Not Enough Meows!";
+        warning.classList.add("warning");
+        document.body.appendChild(warning);
+
+        setTimeout(() => warning.remove(), 1000);
+
+    }
+});
+
+document.querySelector(".upgrade__five").addEventListener("click", () => {
+    if (meows >= upgradeFiveCost) {
+        meows -= upgradeFiveCost;
+        meowsPerClick += 5;
+        upgradeFiveCost = Math.floor(upgradeFiveCost * 1.3);
+        updateCurrency();
+    }
+    else {
+        const warning = document.createElement("p");
+        warning.textContent = "Not Enough Meows!";
+        warning.classList.add("warning");
+        document.body.appendChild(warning);
+
+        setTimeout(() => warning.remove(), 1000);
+
+    }
+});
+
+document.querySelector(".upgrade__six").addEventListener("click", () => {
+    if (meows >= upgradeSixCost) {
+        meows -= upgradeSixCost;
+        meowsPerSecond += 10;
+        upgradeSixCost = Math.floor(upgradeSixCost * 1.4);
+        updateCurrency();
+    }
+    else {
+        const warning = document.createElement("p");
+        warning.textContent = "Not Enough Meows!";
+        warning.classList.add("warning");
+        document.body.appendChild(warning);
+
+        setTimeout(() => warning.remove(), 1000);
+
+    }
+});
+
+
+//meowsPerSecond Loop
+
+setInterval(() => {
+    if (meowsPerSecond > 0) {
+        meows += meowsPerSecond;
+        updateCurrency();
+    }
+}, 1000);
+
+
+//floating +1
+
+function floatingText(amount, x, y) {
+    const float = document.createElement("div");
+    float.classList.add("floating");
+    float.textContent = "+" + amount;
+    float.style.left = x + "px";
+    float.style.top = y + "px";
+    document.body.appendChild(float);
+
+    setTimeout(() => float.remove(), 1000);
+}
+//
+
+
+//handler
+
+function handleCatClick(e) {
+    meows += meowsPerClick;
+    updateCurrency();
+
+    floatingText(meowsPerClick, e.pageX, e.pageY);
+
+    const randomTilt = Math.random() < 0.5 ? -15 : 15;
+    catImg.style.transform = `rotate(${randomTilt}deg)`;
+    
+
+    setTimeout(() => {
+        catImg.style.transform = `rotate(0deg)`;
+    }, 100);
+}
+//
+
+
+ //cheat code
+
+function storeInput() {
+    const userInput = document.querySelector(".user__input").value;
+    return userInput;
+}
+
+function cheatCode() {
+    const code = storeInput();
+
+    if(code === "goated") {
+        meows += 50000000;
+    updateCurrency();
+    }
+}
+
+function clearInput() {
+    document.querySelector(".user__input").value = "";
+}
+//
+
+document.querySelector(".cheat__enter-btn").addEventListener("click", cheatCode)
+document.querySelector(".cheat__enter-btn").addEventListener("click", clearInput)
+catImg.addEventListener("click", handleCatClick);
